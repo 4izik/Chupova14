ecdb34095bf052e9265bb6ed19255b5205dedeb8
4izik <N23492@mail.ru>
2021-06-29 16:33:52 +0500

    ver_4

--- Chupova14/Chupova14/Hero.swift
+++ Chupova14/Chupova14/Hero.swift
@@ -1,26 +1,80 @@
 
 import Foundation
-
-import Foundation
-
-class Hero:Codable {
-    let name: String
-    let status: String
-    let species:String
-    let image: String
-    let id: Int
-    let type:String
-    let gender:String
-    let episode:[String]
-    let url:String
-    let location:Location
-    let origin:Origin
-    let created:String
-
+import RealmSwift
+import Realm
+@objcMembers class Hero: Object, Decodable {
+    @objc dynamic var imageURL=""
+    @objc dynamic var name=""
+    @objc dynamic var status=""
+    let locations = RealmSwift.List<Location>()
+    enum CodingKeys: String, CodingKey {
+            case name = "name"
+            case status = "status"
+            case image = "image"
+            case location = "location"
+        }
+    required init(from decoder: Decoder) throws
+        {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            imageURL = try container.decode(String.self, forKey: .image)
+            name = try container.decode(String.self, forKey: .name)
+            let location = try container.decode([Location].self, forKey: .location)
+            locations.append(objectsIn: location)
+            super.init()
+        }
+    override static func primaryKey() -> String?
+        {
+            return "name"
+        }
+    required init()
+       {
+           super.init()
+       }
+       
+       required init(value: Any, schema: RLMSchema)
+       {
+           super.init(value: value, schema: schema)
+       }
+       
+       required init(realm: RLMRealm, schema: RLMObjectSchema)
+       {
+           super.init(realm: realm, schema: schema)
+       }
 }
-class Location:Codable {
-    let name:String
-    let url:String
+@objcMembers class Location:Object, Decodable {
+    @objc dynamic var name=""
+    @objc dynamic var url=""
+    enum CodingKeys: String, CodingKey {
+            case name = "name"
+            case url = "url"
+        }
+    required init(from decoder: Decoder) throws
+        {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            name = try container.decode(String.self, forKey: .name)
+            url = try container.decode(String.self, forKey: .url)
+            super.init()
+        }
+    override static func primaryKey() -> String?
+        {
+            return "name"
+        }
+    required init()
+       {
+           super.init()
+       }
+       
+       required init(value: Any, schema: RLMSchema)
+       {
+           super.init(value: value, schema: schema)
+       }
+       
+       required init(realm: RLMRealm, schema: RLMObjectSchema)
+       {
+           super.init(realm: realm, schema: schema)
+       }
 }
 class Origin:Codable{
     let name:String
--- Chupova14/Chupova14/HeroRealm.swift
+++ Chupova14/Chupova14/HeroRealm.swift
@@ -1,17 +1,11 @@
 import Foundation
 import RealmSwift
 
-class HeroRealm:Object {
-    @objc dynamic var imageURL=""
-    @objc dynamic var name=""
-    @objc dynamic var status=""
-    @objc dynamic var location=""
-}
 class SaveHeroRealm {
     static let sharedHero = SaveHeroRealm()
     private let realm = try! Realm()
     func addHeroRealm(hero:Hero) {
-        let character=HeroRealm()
+        let character=Hero()
         character.imageURL=hero.image
         character.name=hero.name
         character.status=hero.status + "-" + hero.species
